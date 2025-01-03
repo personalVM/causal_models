aa5d616da460f04e2e9ab5d3daff36894da3d636
Guilherme Viegas <46000780+Gui-go@users.noreply.github.com>
2025-01-03 01:27:41 +0100

    Update sar.R

--- sar.R
+++ sar.R
@@ -4,7 +4,8 @@
 df
 
 library(HSAR)
-
+library(spatialreg)
+?HSAR::sar()
 install.packages("spdep")
 library(spdep)
 dfs_shp
@@ -14,7 +15,7 @@ lw <- nb2listw(nb)
 # neighbors <- poly2nb(your_spatial_data, queen = FALSE) 
 # weights <- nb2listw(neighbors, style = "W")
 
-
+indv = "mean_salary_total + ln_inhabitants_with_higherEducation_total_pc + sg_region" ### Cool!!
 indv = "mean_salary_total + ln_inhabitants_with_higherEducation_total_pc + ln_inhabitants_pkm + sg_region" ### Cool!!
 indv = "mean_salary_total + ln_inhabitants_with_higherEducation_total_pc + sg_region"
 indv = "mean_salary_total + ln_inhabitants_with_higherEducation_total_pc + ln_exports_pc + sg_region"
@@ -29,33 +30,19 @@ imig_form = as.formula(paste0("ln_immigrants_pc  ~ ", indv))
 internal_form = as.formula(paste0("ln_nao_naturais_da_unidade_da_federacao_pc  ~ ", indv))
 
 
-# Fit a SAR model
-spdep::lag
-sar_model <- lagsarlm(emig_form, data = dfs_shp, lw)
-sar.res <- spdep::spautolm(emig_form,listw=lw,data=dfs_shp)
-sar.res
-spdep
-
-
-sar_model <- spdep::spautolm(emig_form, data = dfs_shp, listw = lw, family = "SAR")
-sar_model <- spautolm(ln_emig_total_pc ~ mean_salary_total + ln_inhabitants_with_higherEducation_total_pc + ln_labor_pc + is_coastal_50km + sg_region, data = dfs_shp, listw = lw, family = "SAR")
-sar_model <- spdep::splm(ln_emig_total_pc ~ mean_salary_total + ln_inhabitants_with_higherEducation_total_pc + ln_labor_pc + is_coastal_50km + sg_region, data = dfs_shp, listw = lw, family = "SAR")
-
-# Summary of the model
+library(spatialreg)
+sar_model <- spatialreg::spautolm(emig_form, data = dfs_shp, listw = lw, family = "SAR")
+sar_model <- spatialreg::spautolm(imig_form, data = dfs_shp, listw = lw, family = "SAR")
+sar_model <- spatialreg::spautolm(internal_form, data = dfs_shp, listw = lw, family = "SAR")
 summary(sar_model)
 
-# Check residuals for spatial autocorrelation
-moran.test(residuals(sar_model), lw)
+residuals <- residuals(sar_model)
+tss <- sum((dfs_shp$ln_emig_total_pc - mean(dfs_shp$ln_emig_total_pc))^2)
+tss <- sum((dfs_shp$ln_immigrants_pc - mean(dfs_shp$ln_emig_total_pc))^2)
+rss <- sum(residuals^2)
+r_squared <- 1 - (rss / tss)
+n <- length(residuals)  # Number of observations
+k <- length(sar_model$coefficients) - 1  # Number of predictors (excluding the intercept)
+adjusted_r_squared <- 1 - ((1 - r_squared) * (n - 1)) / (n - k - 1)
 
-
-
-
-
-
-
-RUN curl -o /home/rstudio/HSAR_0_5_1.tar.gz https://cran.r-project.org/src/contrib/Archive/HSAR/HSAR_0.5.1.tar.gz
-RUN tar -xzvf /home/rstudio/HSAR_0_5_1.tar.gz -C /home/rstudio/
-  RUN sudo chmod -R 775 /home/rstudio/HSAR/ && sudo chown -R rstudio:rstudio /home/rstudio/HSAR/
-  RUN R -e "install.packages('/home/rstudio/HSAR/', repos = NULL, type = 'source')"
-
-
+cat("Adjusted R-squared:", adjusted_r_squared, "\n")
