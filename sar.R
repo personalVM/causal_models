b6f0ce653088c28062ccbf4e243a066765220c67
Guilherme Viegas <46000780+Gui-go@users.noreply.github.com>
2025-01-12 18:46:23 +0100

    Update sar.R

--- sar.R
+++ sar.R
@@ -1,16 +1,35 @@
-
-
-
-df
-
 library(HSAR)
 library(spatialreg)
 ?HSAR::sar()
-install.packages("spdep")
+# install.packages("spdep")
 library(spdep)
-dfs_shp
+
+
+
+df <- df %>% 
+  mutate(cd_micro=as.character(cd_micro))
+
+# View(df)
+# df$ln_emig_total_pc
+# df$ln_immigrants_pc
+# df$ln_nao_naturais_da_unidade_da_federacao_pc
+
+
+
+
+
+shp <- sf::st_read(paste0("volume/data/clean_data/micro/shp/")) %>%
+  janitor::clean_names() %>%
+  mutate(across(where(is.numeric), as.character)) %>%
+  select(cd_micro) %>% 
+  sf::st_set_crs(4326)
+dfs_shp <- left_join(df, shp) %>% 
+  filter(cd_micro != 26019) %>% 
+  sf::st_sf()
+
 nb <- poly2nb(dfs_shp, row.names = dfs_shp$cd_micro)
 lw <- nb2listw(nb)
+print("I am Ready!")
 
 # neighbors <- poly2nb(your_spatial_data, queen = FALSE) 
 # weights <- nb2listw(neighbors, style = "W")
@@ -25,16 +44,27 @@ indv = "mean_salary_total + ln_inhabitants_with_higherEducation_total_pc + ln_fi
 indv = "mean_salary_total + ln_inhabitants_with_higherEducation_total_pc + ln_labor_pc + dist_coast"
 indv = "mean_salary_total + ln_inhabitants_with_higherEducation_total_pc + ln_labor_pc + is_coastal_50km" ### Cool!!
 indv = "mean_salary_total + ln_inhabitants_with_higherEducation_total_pc + ln_labor_pc + is_coastal_50km + sg_region" ### Cool!!
+
 emig_form = as.formula(paste0("ln_emig_total_pc  ~ ", indv))
+y = dfs_shp$ln_emig_total_pc
 imig_form = as.formula(paste0("ln_immigrants_pc  ~ ", indv))
+y = dfs_shp$ln_immigrants_pc
 internal_form = as.formula(paste0("ln_nao_naturais_da_unidade_da_federacao_pc  ~ ", indv))
+y = dfs_shp$ln_nao_naturais_da_unidade_da_federacao_pc
 
 
 library(spatialreg)
-sar_model <- spatialreg::spautolm(emig_form, data = dfs_shp, listw = lw, family = "SAR")
-sar_model <- spatialreg::spautolm(imig_form, data = dfs_shp, listw = lw, family = "SAR")
-sar_model <- spatialreg::spautolm(internal_form, data = dfs_shp, listw = lw, family = "SAR")
+(sar_model <- spatialreg::spautolm(emig_form, data = dfs_shp, listw = lw, family = "SAR"))
+(sar_model <- spatialreg::spautolm(imig_form, data = dfs_shp, listw = lw, family = "SAR"))
+(sar_model <- spatialreg::spautolm(internal_form, data = dfs_shp, listw = lw, family = "SAR"))
 summary(sar_model)
+
+fitted_values <- fitted(sar_model)
+residuals <- residuals(sar_model)
+TSS <- sum((y - mean(y))^2)
+RSS <- sum(residuals^2)
+pseudo_r2 <- 1 - (RSS / TSS)
+pseudo_r2
 
 residuals <- residuals(sar_model)
 tss <- sum((dfs_shp$ln_emig_total_pc - mean(dfs_shp$ln_emig_total_pc))^2)
@@ -46,3 +76,7 @@ k <- length(sar_model$coefficients) - 1  # Number of predictors (excluding the i
 adjusted_r_squared <- 1 - ((1 - r_squared) * (n - 1)) / (n - k - 1)
 
 cat("Adjusted R-squared:", adjusted_r_squared, "\n")
+
+
+
+
