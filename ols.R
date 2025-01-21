557432bee95067fd5ee71caca282d805c09af5ad
Guilherme Viegas <46000780+Gui-go@users.noreply.github.com>
2025-01-21 09:05:27 +0100

    Update ols.R

--- ols.R
+++ ols.R
@@ -1,13 +1,34 @@
 source("volume/etl/util_loadPackages.R")
 
-fct_ols = function(ols_form, data){
-  ols_model = lm(as.formula(ols_form), data = data)
+fct_ols = function(form, data){
+  ols_model = lm(as.formula(form), data = data)
   ols_summary <- summary(ols_model)
+  r2 <- ols_summary$r.squared
+  r2adj <- ols_summary$adj.r.squared
+  
+  data$residuals = ols_model$residuals
+  # data$predicted = predicted
+  data$fit = as.vector(ols_model$fitted.values)
+  data$y = as.vector(ols_model$model[, 1])
+  
+  ols_mape <- MLmetrics::MAPE(data$fit, data$y) 
+  rmse <- sqrt(mean((data$y - data$fit)^2))
+  
+  fct_globalMI <- function(var="gama"){
+    mi  = spdep::moran.mc(data[[var]], lw, nsim=599)$statistic %>% as.numeric(.)
+    sig = spdep::moran.mc(data[[var]], lw, nsim=599)$p.value %>% as.numeric(.)
+    return(list(mi=mi, sig=sig))
+  }
   
   return(
     list(
-      ols_model=ols_model,
-      ols_summary=ols_summary
+      model   = ols_model,
+      summary = ols_summary,
+      R2      = r2,
+      R2adj   = r2adj,
+      mape    = ols_mape,
+      rmse    = rmse,
+      fct_globalMI = fct_globalMI
     )
   )
 }
