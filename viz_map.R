35a25579c27d510489d1587049515ad614ce0dbe
Guilherme Viegas <46000780+Gui-go@users.noreply.github.com>
2025-01-27 14:29:20 +0100

    Update viz_map.R

--- viz_map.R
+++ viz_map.R
@@ -1,11 +1,14 @@
-viz_map <- function(data, var_viz = "eci") {
-  gg <- ggplot(data, aes(fill = !!sym(var_viz))) +
+viz_map <- function(data, var_viz = "eci", color="blue") {
+  gg <- ggplot(data %>% sf::st_as_sf(), aes(fill = !!sym(var_viz))) +
     geom_sf(color = "black", size = 0.06) +
-    scale_fill_gradient(low = "white", high = "darkblue") +
-    theme_minimal() +
+    scale_fill_gradient(low = "white", high = color) +
+    # theme_minimal() +
+    theme_void() +
     labs(
       title = paste("Visualization of", var_viz),
       fill = var_viz
     )
   return(gg)
 }
+
+
